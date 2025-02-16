4088826338b3c48813fe257d03617092426fb6e8
Peter Murray <pete@pmurray.com>
2025-02-16 09:28:00 -0500

    adding writer to exports

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 pub const Blowfish = @import("Blowfish.zig");
 pub const BlowfishReader = @import("BlowfishReader.zig");
+pub const BlowfishWriter = @import("BlowfishWriter.zig");
 
 test "All blowfish tests..." {
     std.testing.refAllDecls(@This());
